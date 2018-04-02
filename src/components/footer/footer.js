a826d6d6994c672ab8eff9771c2b4edbed4b2511
Felipe Fontelas <felipefontelas.123@gmail.com>
2018-04-02 10:22:08 -0300

    Home

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom';
 const Footer = () => (
     <footer>
         <div className="col-md-6">
-            <Link className="site-header__menu__item" to="/menu">Sobre o Torresmo Kings®</Link>
-            <Link className="site-header__menu__item" to="/menu">Termos e Condições</Link>
-            <Link className="site-header__menu__item" to="/menu">Trabalhe Conosco</Link>
+            <Link className="site-footer__menu__item" to="/menu">Sobre o Torresmo Kings®</Link>
+            <Link className="site-footer__menu__item" to="/menu">Termos e Condições</Link>
+            <Link className="site-footer__menu__item" to="/menu">Trabalhe Conosco</Link>
         </div>
         <div className="col-sm-4 footer-menu">
             <img alt="Facebook" src="/img/facebook-6-32.png" />
