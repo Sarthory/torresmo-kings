14fdedbb797384782db64bb4e23fa038fa353f48
Felipe Fontelas <felipefontelas.123@gmail.com>
2018-04-02 13:13:46 -0300

    Home

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -8,30 +8,45 @@ const HomePage = () => (
                 <img className="story__img" src="/img/porcao_torresmo_home.png"/>
                 <h1 className="story__title">História</h1>
                 <p className="story__explanation">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus suscipit a augue molestie tincidunt. Morbi non nisi malesuada, hendrerit leo eleifend, hendrerit velit. Pellentesque ut lacus tempor, facilisis felis nec, pretium turpis. Phasellus ut tellus quis mi feugiat interdum. Praesent ullamcorper varius ipsum eu maximus.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus suscipit a augue molestie
+                    tincidunt. Morbi non nisi malesuada, hendrerit leo eleifend, hendrerit velit. Pellentesque ut lacus
+                    tempor, facilisis felis nec, pretium turpis. Phasellus ut tellus quis mi feugiat interdum. Praesent
+                    ullamcorper varius ipsum eu maximus.
                 </p>
                 <div className="story__social-media">
-                    <img alt="Facebook" src="/img/facebook-32-black.png" />
-                    <img alt="Instagram" src="/img/instagram-32-black.png" />
+                    <img alt="Facebook" src="/img/facebook-32-black.png"/>
+                    <img alt="Instagram" src="/img/instagram-32-black.png"/>
                 </div>
             </div>
         </div>
         <div className="col-md-7 home-tiles">
             <div className="row h-50">
-                <div className="col-sm our-stores">
-                    <h1><Link className="our-stores__link" to="/#">Nossas Lojas</Link></h1>
-                </div>
-                <div className="col-sm home-menu">
-                    <h1><Link className="home-menu__link" to="/menu">Cardápio</Link></h1>
-                </div>
+                <Link className="home-tiles__link w-50" to="/#">
+                    <h1>Nossas lojas</h1>
+                    <div className="col-sm">
+                        <img src="/img/our-stores.jpg" />
+                    </div>
+                </Link>
+                <Link className="home-tiles__link w-50" to="/menu">
+                    <h1>Cardápio</h1>
+                    <div className="col-sm">
+                        <img src="/img/menu.jpeg" />
+                    </div>
+                </Link>
             </div>
             <div className="row h-50">
-                <div className="col-sm merchandising">
-                    <h1><Link className="merchandising__link" to="/#">Merchandising</Link></h1>
-                </div>
-                <div className="col-sm events">
-                    <h1><Link className="events__link" to="/#">Eventos</Link></h1>
-                </div>
+                <Link className="home-tiles__link w-50" to="/#">
+                    <h1>Merchandising</h1>
+                    <div className="col-sm">
+                        <img src="/img/merchan.jpg" />
+                    </div>
+                </Link>
+                <Link className="home-tiles__link w-50" to="/#">
+                    <h1>Eventos</h1>
+                    <div className="col-sm">
+                        <img src="/img/events.jpg" />
+                    </div>
+                </Link>
             </div>
         </div>
     </div>
